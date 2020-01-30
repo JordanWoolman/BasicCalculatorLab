ef7f675d117c98787cfaf5ecb87484b95a7773d5
JordanWoolman <59981872+JordanWoolman@users.noreply.github.com>
2020-01-29 22:53:14 -0600

    Update Program.cs

--- BasicCalculatorLab/BasicCalculator/Program.cs
+++ BasicCalculatorLab/BasicCalculator/Program.cs
@@ -7,18 +7,43 @@ namespace BasicCalculator
         static void Main(string[] args)
         {
             Console.WriteLine("*** BASIC CALCULATOR ***");
+            //declare variables 
+            string input;
+            int temp;
+            int relhumidity;
+            int dewPoint;
+            
 
-            Console.WriteLine("Enter the first number");
+            //ask for temp
+            Console.WriteLine("Enter the Temperature in Degrees Fahrenheit");
+            input = Console.ReadLine();
+            temp = int.Parse(input);
 
-            // int.Parse will take a string data type and convert it to an int data type
-            int firstNumber = int.Parse(Console.ReadLine());
+            //ask for humidity 
+            Console.WriteLine("Enter Relative Humidity");
+            input = Console.ReadLine();
+            relhumidity = int.Parse(input);
 
-            Console.WriteLine("Enter the second number");
-            int secondNumber = int.Parse(Console.ReadLine());
+            //calculate the dew point
+            dewPoint = temp - 9 * (100 - relhumidity) / 25;
 
-            int sum = firstNumber + secondNumber;
+            //print the dew point
+            Console.WriteLine("dew point =" + dewPoint);
 
-            Console.WriteLine("The answer is " + sum);
+            //ask for windspeed
+            Console.WriteLine("enter windspeed");
+            input = Console.ReadLine();
+
+            //calculate for windchill
+            double windchill = 35.74;
+            +(0.6125*temperature);
+            -35.75 * Math.Pow(windspeed, .16);
+            +.4275 *temperature * Math.Pow(windspeed, .16)
+            
+            //print the wind chil
+            Console.ReadLine("The Wind Chill is" +windChill)
+
+           
         }
     }
 }
